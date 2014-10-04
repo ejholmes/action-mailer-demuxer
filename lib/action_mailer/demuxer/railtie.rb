1f9321187df3f8630b86c1acb1e18d33f3b95251
Eric J. Holmes <eric@ejholmes.net>
2014-10-03 23:28:54 -0700

    Add in a configure block.

--- lib/action_mailer/demuxer/railtie.rb
+++ lib/action_mailer/demuxer/railtie.rb
@@ -1,10 +1,8 @@
 module ActionMailer
   module Demuxer
     class Railtie < Rails::Railtie
-      initializer 'action_mailer.demuxer.add_delivery_method' do
-        ActiveSupport.on_load :action_mailer do
-          ActionMailer::Base.add_delivery_method :demuxer, ActionMailer::Demuxer::DeliveryMethod
-        end
+      configure do
+        ActionMailer::Base.add_delivery_method :demuxer, ActionMailer::Demuxer::DeliveryMethod
       end
     end
   end
